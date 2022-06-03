2c8e2c6f7f308d7e44442719b5d951bb14028ac7
NicoBarragan <nbarragan01@gmail.com>
2022-06-03 02:51:48 -0300

    feat(raffle.tests.ts): implemented and passed 2 more tests: an event test and other for asserting revertion when raffle is calculating

--- test/unit/raffle.test.ts
+++ test/unit/raffle.test.ts
@@ -6,7 +6,7 @@ import {
     localDeploy,
     KEEPER_INTERVAL,
     ENTRANCE_FEE,
-} from "../../scripts/deploy-local";
+} from "../../scripts/deploy/deploy-local";
 
 if (network.name !== ("hardhat" || "localhost")) {
     describe.skip;
@@ -29,7 +29,7 @@ if (network.name !== ("hardhat" || "localhost")) {
             const { raffleContract, vrfCoordinatorV2contract } =
                 await localDeploy();
 
-            raffle = raffleContract;
+            raffle = raffleContract.connect(ownerWallet);
             vrfCoordinatorV2Mock = vrfCoordinatorV2contract;
         });
 
@@ -48,17 +48,44 @@ if (network.name !== ("hardhat" || "localhost")) {
 
         describe("enterRaffle", async () => {
             it("reverts when you don't pay enough", async () => {
-                // const tx = await raffle.enterRaffle({
-                //     from: ownerWallet,
-                //     value: BigNumber.from((ENTRANCE_FEE / 2).toString()),
-                // });
-                console.log(ENTRANCE_FEE);
-                await expect(raffle.enterRaffle()).to.be.revertedWith(
-                    "Raffle__NotEnoughETHEntered"
-                );
+                await expect(
+                    raffle.enterRaffle({ value: 0 })
+                ).to.be.revertedWith("Raffle__NotEnoughETHEntered");
             });
 
-            it("records players when they enter");
+            it("records players when they enter", async () => {
+                await raffle.enterRaffle({ value: ENTRANCE_FEE });
+                const playerFromContract = await raffle.getPlayer(0);
+                assert.equal(playerFromContract, ownerWallet.address);
+            });
+
+            it("emits event on enter", async () => {
+                await expect(
+                    raffle.enterRaffle({ value: ENTRANCE_FEE })
+                ).to.emit(raffle, "RaffleEnter");
+            });
+
+            it("Doesn't allow entrance when raffle is calculating", async () => {
+                await raffle.enterRaffle({ value: ENTRANCE_FEE });
+
+                // we need to perform upkeep, for that, checkUpkeep must be true
+                // We have 2 conditions passed to be true, enough players and balance, but
+                // 1 of the conditions is that interval must have passed, so for no waiting we need to increase that time
+                const interval = await raffle.getInterval();
+                await network.provider.send("evm_increaseTime", [
+                    interval.toNumber() + 1,
+                ]);
+                // we make real the time increased by mining the block
+                await network.provider.send("evm_mine", []);
+
+                // now we pretend to be the chainlink keeper
+                await raffle.performUpkeep([]);
+
+                // we make the assertion
+                await expect(
+                    raffle.enterRaffle({ value: ENTRANCE_FEE })
+                ).to.be.revertedWith("Raffle__NotOpen");
+            });
         });
     });
 }

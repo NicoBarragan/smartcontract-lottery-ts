fb3381467b87e88d60265994f4ebaeaf4ce6993b
NicoBarragan <nbarragan01@gmail.com>
2022-06-03 02:04:08 -0300

    feat(verify.ts): created function for verifying contract

---
+++ utils/verify.ts
@@ -0,0 +1,29 @@
+import "@nomiclabs/hardhat-ethers";
+import { BigNumber } from "ethers";
+import { run } from "hardhat";
+
+// we can't have these functions in our `helper-hardhat-config`
+// since these use the hardhat library, and it would be a circular dependency
+
+export async function verify(
+    contractAddress: string,
+    args: (string | BigNumber)[]
+) {
+    console.log("Verifying contract...");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (err: any) {
+        if (err.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!");
+        } else {
+            console.log(err);
+        }
+    }
+}
+
+module.exports = {
+    verify,
+};

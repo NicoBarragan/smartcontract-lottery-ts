4da2e653d1d01c8f23f90874c7d48b161c9bc71f
NicoBarragan <nbarragan01@gmail.com>
2022-06-07 17:20:06 -0300

    refactor(deploy-network.ts): removed bad 'verify' function

--- scripts/deploy/deploy-network.ts
+++ scripts/deploy/deploy-network.ts
@@ -1,30 +1,27 @@
-import { ethers, network } from "hardhat";
+import { network } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
-import { deployRaffle } from "./deploy-raffle";
+import { deployRaffle } from "../../utils/deploy-raffle";
 import { BigNumber } from "ethers";
-import { Raffle } from "../../typechain";
-import { verify } from "../../utils/verify";
 const logger = require("pino")();
 
+// get constants from the .env file
 const { VRF_COORDINATOR_V2, KEY_HASH, SUBSCRIPTION_ID, ENTRANCE_FEE } =
     process.env;
 
 // run script for deploy
 export async function networkDeploy() {
+    if (network.name === ("hardhat" || "localhost" || "network")) {
+        logger.error("This script is for testnets or mainnet only");
+    }
+
+    // define constants
     const callbackGasLimit = BigNumber.from("500000");
     const keeperInterval = BigNumber.from(30); // seconds
 
-    let raffleContract: Raffle;
-
-    logger.info(`The network name is ${network.name}`);
-    if (network.name === ("hardhat" || "localhost" || "network")) {
-        logger.warn("This script is for testnets or mainnet only");
-        return;
-    }
-
+    // deploy the raffle contract to a testnet or mainnet
     try {
         logger.info(`Deploying the Raffle contract on the chain...`);
-        raffleContract = await deployRaffle(
+        await deployRaffle(
             BigNumber.from(`${ENTRANCE_FEE}`),
             `${VRF_COORDINATOR_V2}`,
             `${KEY_HASH}`,
@@ -32,25 +29,8 @@ export async function networkDeploy() {
             callbackGasLimit,
             keeperInterval
         );
-        logger.info(`Raffle contract deployed to: ${raffleContract.address}`);
-
-        const args = [
-            BigNumber.from(`${ENTRANCE_FEE}`),
-            `${VRF_COORDINATOR_V2}`,
-            `${KEY_HASH}`,
-            BigNumber.from(`${SUBSCRIPTION_ID}`),
-            callbackGasLimit,
-            keeperInterval,
-        ];
-
-        logger.info("Verifying...");
-        await verify(raffleContract.address, args);
-        logger.info("Verified!");
-
-        return { raffleContract, null: null };
     } catch (err) {
         logger.error(err);
-        process.exitCode = 1;
     }
 }
 
